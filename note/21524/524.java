389e9969e3b90e1fe89333e084849047382ad886
8xue <1667026712@qq.com>
2021-05-24 16:59:33 +0800

    新的note

---
+++ note/21524/524.java
@@ -0,0 +1,10 @@
+//5.24的相关笔记
+原码第一位表示符号位，0为+，1为-
+反码正数和原码一样，负数的话保留符号位1，其他的按位取反
+补码正数和原码一样，负数的是反码+1
+//
+public class 524{
+    public static void main (String[] args){
+        System.out.println(-4);
+        }
+        }
